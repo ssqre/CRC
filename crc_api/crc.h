618a102e55aab0cb36b8db63476ffffe3e396f59
Jie Deng <mr.dengjie@gmail.com>
2015-12-06 12:06:17 +0800

    add crc api

--- crc_api/crc.h
+++ crc_api/crc.h
@@ -1,10 +1,10 @@
-
 #ifndef CRC_H
 #define CRC_H
 
-#include "dsata_types.h"
+#include <inttypes.h>
 
-extern U16 crc16(const void *buffer, U16 length);
-extern U32 crc32(const void *buffer, U16 length);
+extern uint16_t crc16(const void *buffer, uint16_t length, uint16_t init, int lsb);
+
+extern uint32_t crc32(const void *buffer, uint16_t length, uint32_t init, int lsb);
 
 #endif
